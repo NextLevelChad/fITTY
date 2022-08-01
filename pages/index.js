bd553590ee897c265fc6e58db966bd8d2f60fb87
Chad Kennedy <ckennedy9231988@gmail.com>
2022-08-01 13:44:31 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export function HomePage() {
+  return (
+    <div className="container">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
