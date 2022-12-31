2f31c5931440009ce7c5ab94ec761721cf9937f7
NextLevelChad <ckennedy9231988@gmail.com>
2022-12-30 22:13:40 -0600

    removed use session from profile page component

--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,6 @@
 import React from "react";
-import { useSession, signOut } from "next-auth/react";
 
 function profile() {
-  const { data: session } = useSession();
-
-  if (!session) {
-  }
   return <div>profile</div>;
 }
 
