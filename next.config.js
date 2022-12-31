2a7db8fed7313296a3dd3281f377cb720c5e2d8a
NextLevelChad <ckennedy9231988@gmail.com>
2022-12-30 20:57:48 -0600

    removed dot env, upgraded to next 13

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  providers: [
-    {
-      provider: 'google',
-      clientId: 'YOUR_CLIENT_ID',
-      clientSecret: 'YOUR_CLIENT_SECRET',
-    },
-  ],
-  
-}
+};
