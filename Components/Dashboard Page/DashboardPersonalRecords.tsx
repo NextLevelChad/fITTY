99b6efa99a6def80376def73369743005d94ca64
NextLevelChad <ckennedy9231988@gmail.com>
2023-02-25 18:32:23 -0600

    Working on Dashboard data

--- Components/Dashboard Page/DashboardPersonalRecords.tsx
+++ Components/Dashboard Page/DashboardPersonalRecords.tsx
@@ -11,17 +11,35 @@ import ExerciseRecordCard from "./ExerciseRecordCard";
 const DashboardPersonalRecords = () => {
   const { data: session } = useSession();
   const email = session?.user.email;
+  const [personalRecords, setPersonalRecords] = useState(null);
 
-  const personalRecords = trpc.dashboard.getAllPersonalRecords.useQuery({
-    userEmail: email,
-  });
+  useEffect(() => {
+    setPersonalRecords(() => {
+      trpc.dashboard.getAllPersonalRecords.useQuery({
+        userEmail: email,
+      });
+    });
+  }, []);
 
   console.log("These are the returned Personal Records", personalRecords);
 
+  if (!personalRecords) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="shadow-dashboard-card m-4 py-10 px-2 relative justify-center flex flex-col rounded-lg border-2 border-orange-accent max-h-80 max-w-xs bg-color-light-white-fill md:max-w-lg">
-      <TitleBadge text="PERSONAL RECORDS" />
-      <ExerciseRecordCard exerciseName="Barbell Bench Press" type="strength" />
+    <div className="shadow-dashboard-card m-4 py-10 px-2 relative justify-center flex flex-col rounded-lg border-2 border-orange-accent sm:min-h-80 max-w-xs bg-color-light-white-fill md:max-w-lg ">
+      <TitleBadge text="PERSONAL RECORDS..." />
+      <span className="text-center">Your last 5 Personal Records</span>
+      {personalRecords.data?.map((record) => {
+        return (
+          <ExerciseRecordCard
+            exerciseName={record.exerciseName}
+            type="strength"
+            weight={record.weight}
+          />
+        );
+      })}
     </div>
   );
 };
